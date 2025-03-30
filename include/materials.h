2179c39f4a80bea21d114072d3825db59243ad3d
Astha Meena <116489120+10astha@users.noreply.github.com>
2025-03-31 04:00:22 +0530

    added spring_model functions

--- include/materials.h
+++ include/materials.h
@@ -5,26 +5,73 @@
 
 namespace TinyED
 {
-    struct SpringModel{
-
-        static Real getEnergy(const VecS<6> &x, const Real l0, const Real k)
-        {
+    struct SpringModel {
+        static Real getEnergy(const VecS<6> &x, const Real rest_length, const Real k) {
             const Real l = (x.segment<3>(0) - x.segment<3>(3)).norm();
-            return 0.5 * k * (l - l0) * (l - l0);
+            return 0.5 * k * (l - rest_length) * (l - rest_length);
         }
-
-        static void getForce(const VecS<6> &x, const Real l0, const Real k, VecS<6> &force)
-        {
+    
+        static void getForce(const VecS<6> &x, const Real l0, const Real k, VecS<6> &force) {
+            VecS<3> x1 = x.segment<3>(0);
+            VecS<3> x2 = x.segment<3>(3);
+            VecS<3> x21 = x2 - x1;
+            Real l = x21.norm();
+    
+            if (l < 1e-8) {
+                force.setZero();
+              
+            }
+    
+            Real force_magnitude = k * (l - l0);
+            VecS<3> forceVec = force_magnitude * (x21 / l);
+    
+            force.segment<3>(0) = -forceVec; // Force on first point
+            force.segment<3>(3) = forceVec;  // Force on second point
         }
-
-        static void getForceJacobian(const VecS<6> &x, const Real l0, const Real k, MatS<6, 6> &jacobian)
-        {
+    
+        static void get_dFdx(const VecS<6> &x, const Real l0, const Real k, MatS<3, 3> &dFdx) {
+            VecS<3> x1 = x.segment<3>(0);
+            VecS<3> x2 = x.segment<3>(3);
+            VecS<3> x21 = x2 - x1;
+            Real l = x21.norm();
+    
+            if (l < 1e-8) {
+                dFdx.setZero();
+                return;
+            }
+    
+            MatS<3, 3> I = MatS<3, 3>::Identity();
+            MatS<3, 3> x_hat_x_hatT = (x21 * x21.transpose()) / (l * l);
+            dFdx = -k * ((1 - (l0 / l)) * (I - x_hat_x_hatT) + x_hat_x_hatT);
         }
-
-        static void getStress(const VecS<6> &x, const Real l0, const Real k, VecS<3> &stress)
-        {
+    
+        static void getForceJacobian(const VecS<6> &x, const Real l0, const Real k, MatS<6, 6> &jacobian) {
+            MatS<3, 3> dFdx;
+            get_dFdx(x, l0, k, dFdx);
+    
+            jacobian.block<3, 3>(0, 0) = dFdx;
+            jacobian.block<3, 3>(3, 3) = dFdx;
+            jacobian.block<3, 3>(0, 3) = -dFdx;
+            jacobian.block<3, 3>(3, 0) = -dFdx;
+        }
+    
+        static void getStress(const VecS<6> &x, const Real l0, const Real k, VecS<3> &stress) {
+            VecS<3> x1 = x.segment<3>(0);
+            VecS<3> x2 = x.segment<3>(3);
+            VecS<3> x21 = x2 - x1;
+            Real l = x21.norm();
+    
+            if (l < 1e-8) {
+                stress.setZero();
+               
+            }
+    
+            Real strain = (l - l0) / l0;
+            Real stress_magnitude = k * strain;
+            stress = stress_magnitude * (x21 / l);
         }
     };
+    
 
     struct StVKModel
     {
@@ -44,6 +91,10 @@ namespace TinyED
 
         }
     };
+
+
+
+
 
     struct NeoHookeanModel
     {
