cfa14047122be2f2861934217a7aaa6aa0f5f4ec
Astha Meena <116489120+10astha@users.noreply.github.com>
2025-04-09 05:29:17 +0530

    Update materials.h

--- include/materials.h
+++ include/materials.h
@@ -85,7 +85,9 @@ namespace TinyED
         
             // Compute Deformation Gradient
             MatS<3,2> F;
-            computeDeformationGradient(positions, invRestMat, F);
+            MatS<3,2> deformedCoordsToDeformationGradient;
+            computeDeformationGradient(positions, invRestMat, deformedCoordsToDeformationGradient, F);
+         
         
             // Compute Green Strain
             MatS<2,2> greenStrain = (F.transpose() * F - MatS<2,2>::Identity()) * 0.5;
@@ -97,24 +99,109 @@ namespace TinyED
         
             return energy;
         }
-
-        static void getForce(const VecS<9>& x, const Real area, const Real lambda, const Real mu, VecS<9>& force)
+      
+        static void get_stvk_stress(
+                const Real mu, const Real lambda, const MatS<3, 2>& F, MatS<3, 2>& sigma)
         {
+            MatS<2,2> greenStrain = (F.transpose() * F - MatS<2, 2>::Identity()) * 0.5;
+            // St. Venant-Kirchhoff model
+            sigma = F * (2 * mu * greenStrain + lambda * greenStrain.trace() * MatS<2,2>::Identity());
+        }
+        static void get_stvk_Force(
+            const VecS<9>& x,
+                const MatS<2,2>& invRestMat,
+                const Real restVolume,
+                const Real mu, const Real lambda,
+                
+                VecS<9>& force)
+        {
+            MatS<3, 2> sigma;
 
+            MatS<3,2> deformedCoordsToDeformationGradient;
+            MatS<3,2> F;
+
+            computeDeformationGradient(x, invRestMat, deformedCoordsToDeformationGradient, F);
+
+            get_stvk_stress(mu, lambda, F, sigma);
+            
+            auto force_map = Eigen::Map<MatS<3, 3>>(reinterpret_cast<Real*>(force.data())); 
+            force_map = sigma * deformedCoordsToDeformationGradient.transpose() * restVolume;
+            force = -force;
         }
 
-        static void getForceJacobian(const VecS<9>& x, const Real area, const Real lambda, const Real mu, MatS<9, 9>&
-        force)
+
+        // static void getstvkForceJacobian(const VecS<9>& x, const Real area, const Real lambda, const Real mu, MatS<9, 9>&
+        // force)
+        // {
+
+        // }
+        static void getstvkForceJacobians(
+            const VecS<9>& positions, 
+            const MatS<2, 2>& invRestMat, 
+            const Real volume, 
+            const Real mu, 
+            const Real lambda, 
+            VecS<9>& force, 
+            MatS<9, 9>& forcesPositionDeriv)
         {
-
+            get_stvk_Force(positions, invRestMat, volume, mu, lambda,  force);
+        
+            MatS<3, 2> deformedCoordsToDeformationGradient;
+            MatS<3, 2> F;
+            computeDeformationGradient(positions, invRestMat, deformedCoordsToDeformationGradient, F);
+            const MatS<2, 3> BT = deformedCoordsToDeformationGradient.transpose();
+            const MatS<2, 2> greenStrain = (F.transpose() * F - MatS<2, 2>::Identity()) * 0.5;
+        
+            const Real greenStrainTrace = greenStrain.trace();
+            const Real lambdaGreenTrace = lambda * greenStrainTrace;
+        
+            for (int s = 0; s < 2; s++) {
+                const Eigen::Block<MatS<3, 2>, 3, 1, true> delF_rsdelx_j = deformedCoordsToDeformationGradient.col(s);
+        
+                for (int l = 0; l < 2; l++) {
+                    Real delP_ildelF_rs_p1_cached = 2 * mu * greenStrain(s, l);
+                    if (s == l) {
+                        delP_ildelF_rs_p1_cached += lambdaGreenTrace;
+                    }
+        
+                    for (int r = 0; r < 3; r++) {
+                        Real deltrEdelF_rs = F(r, s);
+                        MatS<2, 1> delE_kldelF_rs_p1 = MatS<2, 1>::Zero();
+        
+                        if (l == s) {
+                            for (int k = 0; k < 2; k++) {
+                                delE_kldelF_rs_p1(k) = F(r, k);
+                            }
+                        }
+        
+                        for (int i = 0; i < 3; i++) {
+                            Real delP_ildelF_rs_p1 = (i == r) ? delP_ildelF_rs_p1_cached : 0.0;
+                            Real delP_ildelF_rs_p2 = 0.0;
+        
+                            if (l == s) {
+                                delP_ildelF_rs_p2 += mu * F.row(i).dot(delE_kldelF_rs_p1);
+                            }
+        
+                            delP_ildelF_rs_p2 += F(i, s) * mu * F(r, l);
+                            delP_ildelF_rs_p2 += F(i, l) * lambda * deltrEdelF_rs;
+        
+                            Real delP_ildelF_rs = delP_ildelF_rs_p1 + delP_ildelF_rs_p2;
+        
+                            for (int x = 0; x < 3; x++) {
+                                for (int y = 0; y < 3; y++) {
+                                    forcesPositionDeriv(i + 3 * x, r + 3 * y) += 
+                                        -volume * delP_ildelF_rs * delF_rsdelx_j(y) * deformedCoordsToDeformationGradient(x, l);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
-    };
-
-
-
-
-
-    struct NeoHookeanModel
+        
+      
+};       
+     struct NeoHookeanModel
     {
         static Real getEnergy(const VecS<12>& x, const Real area, const Real lambda, const Real mu)
         {
@@ -136,3 +223,9 @@ namespace TinyED
 }
 
 #endif //MATERIALS_H
+
+
+
+
+
+
