b2e4a420e8c8ccf2b52508a8c30ceca88baf47f4
Astha Meena <116489120+10astha@users.noreply.github.com>
2025-05-09 15:24:28 +0530

    Update helpers.h

--- include/helpers.h
+++ include/helpers.h
@@ -4,38 +4,7 @@
 #include <iostream>
 namespace TinyED
 {
-    // void computeInvRestMat(const VecS<9>& rest_vertices , MatS<2, 2>& invRestMat) {
-    // //2d triangle
-    //     // Define transformation matrix H
-        
-    //     MatS<3, 2> H;
-    //     H << -1, -1,
-    //           1,  0,
-    //           0,  1;
-    
-    //     // Compute rest shape matrix X
-    //     MatS<3, 2> X;
-    //     if (rest_vertices[1] == rest_vertices[4] && rest_vertices[4] == rest_vertices[7]) {
-    //         // All y-coordinates are the same → XZ plane
-    //         X << rest_vertices[0], rest_vertices[2],  // Vertex 0 (x, z)
-    //              rest_vertices[3], rest_vertices[5],  // Vertex 1 (x, z)
-    //              rest_vertices[6], rest_vertices[8];  // Vertex 2 (x, z)
-    //     } else if (rest_vertices[2] == rest_vertices[5] && rest_vertices[5] == rest_vertices[8]) {
-    //         // All z-coordinates are the same → XY plane
-    //         X << rest_vertices[0], rest_vertices[1],  // Vertex 0 (x, y)
-    //              rest_vertices[3], rest_vertices[4],  // Vertex 1 (x, y)
-    //              rest_vertices[6], rest_vertices[7];  // Vertex 2 (x, y)
-    //     } else {
-    //         throw std::runtime_error("Only XY and XZ rest alignment are supported");
-    //     }
-    //     // Compute inverse of X * H
-    //     invRestMat =  (X.transpose() * H).inverse();
-
-    //     //print X and H too
-    //     std::cout << "Rest shape matrix X:\n" << X << std::endl;
-    //     std::cout << "Transformation matrix H:\n" << H << std::endl;
-       
-    // }
+   
 template <unsigned int N>
 void computeInvRestMat(const VecS< N * 3>& rest_vertices, MatS<N - 1, N - 1>& invRestMat) {
     static_assert(N == 3 || N == 4, "Only triangles (N=3) and tetrahedra (N=4) are supported");
