25a2fb7db94764d33054cb04094a5b02fbb6afc5
Astha Meena <116489120+10astha@users.noreply.github.com>
2025-05-09 15:24:48 +0530

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,58 +1,92 @@
 #include <iostream>
 #include "materials.h"
-#include "examples_util.h"
 
 using namespace TinyED;
-int main()
-{
-    constexpr int TOTAL_FRAMES = 500;
-    constexpr Real timeStep = 0.01;
-    constexpr Real density = 1.0;
-    constexpr int tesselation = 8;
-    const Vec3 gravity = {0, -9.81, 0};
 
-    auto [triangles, positions, uvs] = generateTesselatedSquare({0, 0, 0}, tesselation, 1.0);
-    const std::vector<Mat2> restMatrices = computeRestShape(positions, uvs, triangles);
-    const std::vector<Real> restVolumes = computeRestVolume(uvs, triangles);
+// int main()
+// {
+//     // Define the positions of two points in 3D space (flattened into VecS<6>)
+//     VecS<6> x;
+//     x << 2, 0,0,   // First point (x1, y1, z1)
+//          0, 0, 0;   // Second point (x2, y2, z2)
 
-    std::vector<Material> matData(triangles.size());
-    std::vector<Element> elements(triangles.size());
+//     Real l0 = 1.0;  // Rest length
+//     Real k = 100.0; // Stiffness constant
 
-    for (size_t i = 0; i < triangles.size(); ++i)
-    {
-        matData[i].type = MaterialType::STVK;
-        matData[i].data = FEMTriData{};
-        std::get<FEMTriData>(matData[i].data).mu = 100.0;
-        std::get<FEMTriData>(matData[i].data).lambda = 150.0;
-        std::get<FEMTriData>(matData[i].data).restVolume = restVolumes[i];
-        std::get<FEMTriData>(matData[i].data).invRestMat = restMatrices[i];
+//     // Compute energy
+//     const Real energy = SpringModel::getEnergy(x, l0, k);
+//     std::cout << "Computed energy: " << energy << std::endl;
 
-        elements[i].elementData = triangles[i];
-    }
+//     // Compute force
+//     VecS<6> force;
+//     SpringModel::getForce(x, l0, k, force);
+//     std::cout << "Computed force: " << force.transpose() << std::endl;
 
-    std::vector<unsigned int> constraintIndices = {0, tesselation};
+//     // Compute force Jacobian
+//     MatS<6, 6> jacobian;
+//     SpringModel::getForceJacobian(x, l0, k, jacobian);
+//     std::cout << "Computed force Jacobian:\n" << jacobian << std::endl;
 
-    const VecN masses = computeVertexMasses(positions, triangles, density);
-    VecN invMasses = 1.0 / masses.array();
+//     // Compute stress
+//     VecS<3> stress;
+//     SpringModel::getStress(x, l0, k, stress);
+//     std::cout << "Computed stress: " << stress.transpose() << std::endl;
+//     //compute DF/dx
+//     MatS<3, 3> dFdx;
+//     SpringModel::get_dFdx(x, l0, k, dFdx);
+//     std::cout << "Computed dF/dx:\n" << dFdx << std::endl;
 
-    const int N = positions.cols();
+  
+//     return 0;
+// }
 
-    Mat3N velocities(3, N);
-    velocities.setZero();
+int main() {
+    // Define rest state (undeformed tetrahedron)
+    VecS<12> rest_vertices;
+    rest_vertices << 0, 0, 0,   // Vertex 0
+                     1, 0, 0,   // Vertex 1
+                     0, 1, 0,   // Vertex 2
+                     0, 0, 1;   // Vertex 3
 
+    // Define deformed state (stretched tetrahedron)
+    VecS<12> deformed_vertices;
+    deformed_vertices  << 0, 0, 0,   // Vertex 0
+                     1.1, 0, 0,   // Vertex 1
+                     0, 1.0, 0,   // Vertex 2
+                     0, 0, 1.1; 
 
-    VecN invMlumped(3*N);
-    for (size_t i = 0; i < N; i++)
-    {
-        if (std::find(constraintIndices.begin(), constraintIndices.end(), i) != constraintIndices.end())
-            invMasses(i) = 0;
-        invMlumped.segment<3>(i*3).setConstant(invMasses(i));
-    }
+    // Material properties
+    Real lambda = 1.0;
+    Real mu = 0.5;
+    Real volume = 1.0 / 6.0;  // Tetrahedron rest volume (simple unit tetrahedron)
 
-    for (int frame = 0; frame < TOTAL_FRAMES; frame++)
-    {
-        advanceStep(positions, velocities, timeStep, elements, matData, invMlumped, masses, gravity);
-        const std::string filename = "frame_" + std::to_string(frame);
-        saveSheet(filename, positions, triangles);
-    }
+    // Compute inverse rest shape matrix
+    MatS<3, 3> invRestMat;
+    computeInvRestMat<4>(rest_vertices, invRestMat);
+    std::cout << "Inverse Rest Shape Matrix:\n" << invRestMat << std::endl;
+
+    // Compute energy
+    Real energy = NeoHookeanModel::getEnergy(deformed_vertices, invRestMat, volume, mu, lambda);
+    std::cout << "Neo-Hookean Energy: " << energy << std::endl;
+
+    // // Compute deformation gradient
+    MatS<3, 3> F;
+    // computeDeformationGradient<4>(deformed_vertices, invRestMat, dXInv, F);
+
+    // // Compute stress
+    MatS<3, 3> sigma;
+    NeoHookeanModel::getStress(deformed_vertices, invRestMat, mu, lambda,sigma);
+    std::cout << "Neo-Hookean Stress:\n" << sigma << std::endl;
+   
+    // Compute force
+    VecS<12> force;
+    NeoHookeanModel::getForce<4>(deformed_vertices, invRestMat, volume, mu, lambda, force);
+    std::cout << "Neo-Hookean Force Vector:\n" << force.transpose() << std::endl;
+
+    // Compute force Jacobian
+    MatS<12, 12> J;
+    NeoHookeanModel::getForceJacobian(deformed_vertices, invRestMat, volume, mu, lambda, force, J);
+    std::cout << "Neo-Hookean Force Jacobian:\n" << J << std::endl;
+
+    return 0;
 }
